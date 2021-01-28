9b52d01df5c6f093f5a9fbe82a0f4afbd213dc41
Nils Witt <nils.witt1901@gmail.com>
2021-01-28 19:40:13 +0100

    news

--- low-quality-code/ewert-varianz.cc
+++ low-quality-code/ewert-varianz.cc
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <fstream>
 
-double varianz(double n, double m){
-    return (std::pow(m-1,2*n))/(std::pow(m,2*n-2))*(m*(m-1)-1)+(std::pow(m-1,n+1))/std::pow(m,n-2); 
-}
+
 
 double mean(int n, int m){
     return (std::pow(m-1,n))/(std::pow(m,n-1));
+}
+
+double varianz(double n, double m, double mu){
+    return mu*(1-m*mean(n,m));
 }
 
 int main(){
@@ -16,7 +18,7 @@ int main(){
     out << "Jaeger" << " " << "Enten" << " " << "mu" << " " << "sigma^2" << std::endl;
     for(int i = 1; i <= 200; ++i){
         for(int j = 10; j <= 10; ++j){
-            out << i << " " << j << " " << mean(i,j) << " " << varianz(i,j) << " " << std::endl; 
+            out << i << " " << j << " " << mean(i,j) << " " << varianz(i,j,mean(i,j)) << " " << std::endl; 
         }
     }
     out.close();
